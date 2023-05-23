81c7a36bf2a5bfe423c427b6f375454efb24868b
Jahleel-cito <jahleel.solano@julius2grow.com>
2023-05-23 17:23:10 -0500

    bug fixed

--- pages/api/cron/script.js
+++ pages/api/cron/script.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 const { ACCESS_KEY } = process.env
 
 export default function script(req, res) {
-    axios.get(`/api?access=${ACCESS_KEY}`).then((result) => {
+    axios.get(`http://localhost:3000/api?access=${ACCESS_KEY}`).then((result) => {
 
         console.log(result);
     }).catch((err) => {
--- pages/api/index.js
+++ pages/api/index.js
@@ -12,13 +12,4 @@ export default function handler(req, res) {
     else {
         res.status(403).send('Acceso no autorizado.');
     }
-    // Verificar la seguridad con la llave de acceso
-    if (acceso === ACCESS_KEY) {
-        script()
-        console.log("did it!")
-        res.status(200).send("oki doki")
-    }
-    else {
-        res.status(403).send('Acceso no autorizado.');
-    }
 }

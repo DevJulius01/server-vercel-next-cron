643c57c846fefb6a0b10ad77f94a5874500d3808
Jahleel-cito <jahleel.solano@julius2grow.com>
2023-05-23 18:07:43 -0500

    bug 4

--- pages/api/cron/script.js
+++ pages/api/cron/script.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 const { ACCESS_KEY } = process.env
 
 export default function script(req, res) {
-    axios.get('https://server-vercel-next-cron.vercel.app/api?success=1234')
+    axios.get('https://server-vercel-next-cron.vercel.app/api?access=1234')
         .then(data => { console.log(data) })
         .catch(
             (err) => {
